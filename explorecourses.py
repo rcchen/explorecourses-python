f88cc7bc574105b6216dc68c54b5e18eab4198ee
Roger Chen <rogerthechen@gmail.com>
2014-03-26 22:31:45 +0100

    Handles sections and schedules now

--- explorecourses.py
+++ explorecourses.py
@@ -1,16 +1,46 @@
 from xml.dom import minidom
 import urllib2
 
+class Schedule:
+
+	def __init__(self, startDate, endDate, startTime, endTime, location, days):
+
+		self.startDate = startDate
+		self.endDate = endDate
+		self.startTime = startTime
+		self.endTime = endTime
+		self.location = location
+		self.days = days
+
+class Section:
+
+	def __init__(self, classId, term, subject, code, units, sectionNumber, component, courseId):
+
+		self.classId = classId
+		self.term = term
+		self.subject = subject
+		self.code = code
+		self.units = units
+		self.sectionNumber = sectionNumber
+		self.component = component
+		self.courseId = courseId
+		self.schedules = []
+
 class Course:
 
-	def __init__(self, year, subject, code, title, description):
+	def __init__(self, year, subject, code, title, description, gers, repeatable, grading, unitsMin, unitsMax):
 
 		self.year = year
 		self.subject = subject
 		self.code = code
 		self.title = title
 		self.description = description
-		self.rank = 0
+		self.gers = gers
+		self.repeatable = repeatable
+		self.grading = grading
+		self.unitsMin = unitsMin
+		self.unitsMax = unitsMax
+		self.sections = []
 
 	@property
 	def fullCode(self):
@@ -48,15 +78,79 @@ class ExploreCourses:
 			code = self.getText(dom_course, 'code')
 			title = self.getText(dom_course, 'title')
 			description = self.getText(dom_course, 'description')
+			gers = self.getText(dom_course, 'gers')
+			courseId = self.getText(dom_course, 'courseId')
+			repeatable = self.getText(dom_course, 'repeatable')
+			grading = self.getText(dom_course, 'grading')
+			unitsMin = self.getText(dom_course, 'unitsMin')
+			unitsMax = self.getText(dom_course, 'unitsMax')
 
 			# Create a new Course
-			course = Course(year, subject, code, title, description)
+			course = Course(year, subject, code, title, description, gers, repeatable, grading, unitsMin, unitsMax)
+
+			# Get the sections
+			dom_sections = dom_course.getElementsByTagName('section')
+
+			# Create section objects
+			for dom_section in dom_sections:
+
+				# Create the section
+				section = self.handleSection(dom_section)
+
+				# Add it to our array
+				course.sections.append(section)
 
 			# Save it into our array
 			courses.append(course)
 
 		# Return the object array
 		return courses
+
+	def handleSection(self, dom_section):
+
+		# Extract data from the DOM
+		classId = self.getText(dom_section, 'classId')
+		term = self.getText(dom_section, 'term')
+		subject = self.getText(dom_section, 'subject')
+		code = self.getText(dom_section, 'code')
+		units = self.getText(dom_section, 'units')
+		sectionNumber = self.getText(dom_section, 'sectionNumber')
+		component = self.getText(dom_section, 'component')
+		courseId = self.getText(dom_section, 'courseId')
+
+		# Create a new section
+		section = Section(classId, term, subject, code, units, sectionNumber, component, courseId)
+
+		# Get the schedules
+		dom_schedules = dom_section.getElementsByTagName('schedule')
+
+		# Create schedule objects
+		for dom_schedule in dom_schedules:
+
+			# Create the schedule
+			schedule = self.handleSchedule(dom_schedule)
+
+			# Add it to our array
+			section.schedules.append(schedule)
+
+		# Return our new instance
+		return section
+
+	def handleSchedule(self, dom_schedule):
+
+		# Extract data from the DOM
+		startDate = self.getText(dom_schedule, 'startDate')
+		endDate = self.getText(dom_schedule, 'endDate')
+		startTime = self.getText(dom_schedule, 'startTime')
+		endTime = self.getText(dom_schedule, 'endTime')
+		location = self.getText(dom_schedule, 'location')
+		days = self.getText(dom_schedule, 'days')
+
+		# Create a new schedule
+		schedule = Schedule(startDate, endDate, startTime, endTime, location, days)
+
+		# Return our new instance
+		return schedule
 
 	# Retrieves the best match for a course
 	def getCourse(self, course):
@@ -85,9 +179,16 @@ class ExploreCourses:
 		return bestResult
 
 	# Get the text from a tag element
-	def getText(self, course, element):
+	def getText(self, dom_object, element):
 
-		return course.getElementsByTagName(element)[0].firstChild.nodeValue
+		# Attempt to fetch the text target
+		target = dom_object.getElementsByTagName(element)[0].firstChild
+
+		# Check to see if the target exists
+		if target is not None:
+			return target.nodeValue
+		else:
+			return ""		
 
 	# Calculates the longest common substring between two strings
 	def longestCommonSubstring(self, str1, str2):
